2c6b2a6022cea7eea0d2d3ff0eb23710faa97350
LukaszSieniec <lukasz.sieniec@tlen.pl>
2020-05-12 19:06:54 +0200

    create calculate method

--- app/src/main/java/com/example/view/distance/view/MainActivity.java
+++ app/src/main/java/com/example/view/distance/view/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.view.distance.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
 import com.example.view.distance.R;
+import com.example.view.distance.model.AddressResponse;
+import com.example.view.distance.viewmodel.ViewModelMainActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -36,6 +41,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TextView textViewFinish;
     @BindView(R.id.textViewDistance)
     TextView textViewDistance;
+
+    private ViewModelMainActivity viewModelMainActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,6 +79,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void calculate() {
-        //TODO
+        String firstLatitude = editTextFirstLatitude.getText().toString();
+        String firstLongitude = editTextFirstLongitude.getText().toString();
+        String secondLatitude = editTextSecondLatitude.getText().toString();
+        String secondLongitude = editTextSecondLongitude.getText().toString();
+
+        if(firstLatitude.trim().isEmpty() || firstLongitude.trim().isEmpty() || secondLatitude.trim().isEmpty() || secondLongitude.trim().isEmpty()) {
+            Toast.makeText(this, "You haven't entered all the data!", Toast.LENGTH_LONG).show();
+
+        } else {
+
+            viewModelMainActivity = new ViewModelProvider(this).get(ViewModelMainActivity.class);
+            viewModelMainActivity.getAddressResponse().observe(this, new Observer<AddressResponse>() {
+                @Override
+                public void onChanged(AddressResponse addressResponse) {
+
+                    Log.i("Test", addressResponse.getCity());
+                }
+            });
+        }
     }
 }
